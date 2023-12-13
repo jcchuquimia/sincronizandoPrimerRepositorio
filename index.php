766328cc67583c3c38f4652c9f5cede1d4db4536
Juan Carlos <jcchuquimia@gmail.com>
2023-12-13 17:26:41 -0400

    solucionando conflictos uno

--- index.php
+++ index.php
@@ -1,17 +1,3 @@
-<<<<<<< HEAD
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
-
-=======
 <header>
   <ol>
     <li><a href="#">Home</a></li>
@@ -20,4 +6,4 @@
     <li><a href="#">Contacto</a></li>
   </ol>
 </header>
->>>>>>> 3da595e9ffede6f139b88c08c789234e1fcab7c7
+
